75582b35b3bbef05e498f7688fd507d7e1e8bbca
Vladimir <Zionit27@gmail.com>
2021-06-03 20:32:20 +0700

    enter variables.

--- app/src/main/java/ru/geekbrains/mycalculator/MainActivity.java
+++ app/src/main/java/ru/geekbrains/mycalculator/MainActivity.java
@@ -3,8 +3,12 @@ package ru.geekbrains.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +16,32 @@ public class MainActivity extends AppCompatActivity {
     TextView operation;     //текстовое поле для вывода операции
     TextView answer;        //текстовое поле для вывода ответа
 
+    Button button_1;
+    Button button_2;
+    Button button_3;
+    Button button_4;
+    Button button_5;
+    Button button_6;
+    Button button_7;
+    Button button_8;
+    Button button_9;
+    Button button_0;
+    Button button_point;
+    Button button_ac;
+    Button button_percent;
+    Button button_division;
+    Button button_multiplication;
+    Button button_subtraction;
+    Button button_addition;
+    Button button_equal_mark;
+
+    double num1 = 0;
+    double num2 = 0;
+    double calc = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        answer = findViewById(R.id.answer);
-        input_value = findViewById(R.id.input_value);
-        operation = findViewById(R.id.operation);
     }
 }
