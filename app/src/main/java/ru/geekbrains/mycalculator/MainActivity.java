ba1549be0578c7b7b943a0b2ab612ee47b09f047
Vladimir <Zionit27@gmail.com>
2021-06-06 14:29:13 +0700

    add method setNumberButtonListeners.

--- app/src/main/java/ru/geekbrains/mycalculator/MainActivity.java
+++ app/src/main/java/ru/geekbrains/mycalculator/MainActivity.java
@@ -16,16 +16,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView operation;     //текстовое поле для вывода операции
     TextView answer;        //текстовое поле для вывода ответа
 
-    Button button_1;
-    Button button_2;
-    Button button_3;
-    Button button_4;
-    Button button_5;
-    Button button_6;
-    Button button_7;
-    Button button_8;
-    Button button_9;
-    Button button_0;
+    private final int[] numberButtonIds = new int[]{R.id.button_0, R.id.button_1, R.id.button_2, R.id.button_3,
+            R.id.button_4, R.id.button_5, R.id.button_6, R.id.button_7, R.id.button_8, R.id.button_9};
     Button button_point;
     Button button_ac;
     Button button_percent;
@@ -48,16 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         input_value = findViewById(R.id.input_value);
         operation = findViewById(R.id.operation);
 
-        button_1 = findViewById(R.id.button_1);
-        button_2 = findViewById(R.id.button_2);
-        button_3 = findViewById(R.id.button_3);
-        button_4 = findViewById(R.id.button_4);
-        button_5 = findViewById(R.id.button_5);
-        button_6 = findViewById(R.id.button_6);
-        button_7 = findViewById(R.id.button_7);
-        button_8 = findViewById(R.id.button_8);
-        button_9 = findViewById(R.id.button_9);
-        button_0 = findViewById(R.id.button_0);
+        setNumberButtonListeners();
         button_point = findViewById(R.id.button_point);
         button_ac = findViewById(R.id.button_ac);
         button_percent = findViewById(R.id.button_percent);
@@ -67,16 +50,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button_addition = findViewById(R.id.button_addition);
         button_equal_mark = findViewById(R.id.button_equal_mark);
 
-        button_1.setOnClickListener(this);
-        button_2.setOnClickListener(this);
-        button_3.setOnClickListener(this);
-        button_4.setOnClickListener(this);
-        button_5.setOnClickListener(this);
-        button_6.setOnClickListener(this);
-        button_7.setOnClickListener(this);
-        button_8.setOnClickListener(this);
-        button_9.setOnClickListener(this);
-        button_0.setOnClickListener(this);
         button_point.setOnClickListener(this);
         button_ac.setOnClickListener(this);
         button_percent.setOnClickListener(this);
@@ -158,7 +131,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
     }
-    public void initNum1 () {
+
+    public void initNum1() {
         num1 = Double.parseDouble(input_value.getText().toString());
     }
+
+    private void setNumberButtonListeners() {
+        int i;
+        for (i = 0; i < 10; i++) {
+            findViewById(numberButtonIds[i]).setOnClickListener(v -> {
+                Button btn = (Button) v;
+                input_value.append(btn.getText().toString());
+            });
+        }
+    }
+
 }
