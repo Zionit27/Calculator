13b8856abe9397ac4002c8fec6e37a9ce9656ff5
Vladimir <Zionit27@gmail.com>
2021-06-10 18:10:27 +0700

    add activity SettingThemeActivity.

---
+++ app/src/main/java/ru/geekbrains/mycalculator/SettingThemeActivity.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.mycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingThemeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_theme);
+    }
+}
