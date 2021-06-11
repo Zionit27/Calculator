f17c80075a6236c059f9692bce2a5a5c8a4568cf
Vladimir <Zionit27@gmail.com>
2021-06-11 17:07:26 +0700

    edit SettingThemeActivity.

--- app/src/main/java/ru/geekbrains/mycalculator/SettingThemeActivity.java
+++ app/src/main/java/ru/geekbrains/mycalculator/SettingThemeActivity.java
@@ -14,14 +14,15 @@ public class SettingThemeActivity extends AppCompatActivity {
 
     private static final String NameSharedPreference = "LOGIN";
     private static final String AppTheme = "APP_THEME";
-    private static final int AppThemeLightCodeStyle = 0;
-    private static final int AppThemeDarkCodeStyle = 1;
-
+    private static final int MyCodeStyle = 0;
+    private static final int AppThemeLightCodeStyle = 1;
+    private static final int AppThemeCodeStyle = 2;
+    private static final int AppThemeDarkCodeStyle = 3;;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(getAppTheme(R.style.AppThemeLight));
+        setTheme(getAppTheme(R.style.MyStyle));
         setContentView(R.layout.activity_setting_theme);
 
         initThemeChooser();
@@ -33,17 +34,19 @@ public class SettingThemeActivity extends AppCompatActivity {
                 finish();
             }
         });
-
     }
 
     private void initThemeChooser() {
-        initRadioButton(findViewById(R.id.radioButtonMaterialLight),
-                AppThemeLightCodeStyle);
+        initRadioButton(findViewById(R.id.radioButtonMyCoolStyle),
+                MyCodeStyle);
         initRadioButton(findViewById(R.id.radioButtonMaterialDark),
                 AppThemeDarkCodeStyle);
+        initRadioButton(findViewById(R.id.radioButtonMaterialLight),
+                AppThemeLightCodeStyle);
+        initRadioButton(findViewById(R.id.radioButtonMaterialLightDarkAction),
+                AppThemeCodeStyle);
         RadioGroup rg = findViewById(R.id.radioButtons);
-
-        ((MaterialRadioButton) rg.getChildAt(getCodeStyle(AppThemeLightCodeStyle))).setChecked(true);
+        ((MaterialRadioButton)rg.getChildAt(getCodeStyle(MyCodeStyle))).setChecked(true);
     }
 
     private void initRadioButton(View button, final int codeStyle) {
@@ -72,12 +75,18 @@ public class SettingThemeActivity extends AppCompatActivity {
         editor.apply();
     }
 
-    private int codeStyleToStyleId(int codeStyle) {
-        if (codeStyle == AppThemeLightCodeStyle) {
-            return R.style.AppThemeLight;
-        } else {
-            return R.style.AppThemeDark;
+    private int codeStyleToStyleId(int codeStyle){
+        switch(codeStyle){
+            case AppThemeCodeStyle:
+                return R.style.AppTheme;
+            case AppThemeLightCodeStyle:
+                return R.style.AppThemeLight;
+            case AppThemeDarkCodeStyle:
+                return R.style.AppThemeDark;
+            default:
+                return R.style.MyStyle;
         }
     }
+
 }
 
