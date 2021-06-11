575708b858e3370a8103619bbcafc16937fe5c17
Vladimir <Zionit27@gmail.com>
2021-06-11 14:30:42 +0700

    add radio buttons.

--- app/src/main/java/ru/geekbrains/mycalculator/SettingThemeActivity.java
+++ app/src/main/java/ru/geekbrains/mycalculator/SettingThemeActivity.java
@@ -9,29 +9,28 @@ import android.widget.TextView;
 
 public class SettingThemeActivity extends AppCompatActivity {
 
-    TextView themeText;
+    private static final String NameSharedPreference = "LOGIN";
+
+    // Имя параметра в настройках
+
+
+    private static final String appTheme = "APP_THEME";
+    private static final int MyCoolCodeStyle = 0;
+    private static final int AppThemeLightCodeStyle = 1;
+    private static final int AppThemeCodeStyle = 2;
+    private static final int AppThemeDarkCodeStyle = 3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_theme);
 
-        themeText = findViewById(R.id.theme_text);
-
         Button buttonReturn = findViewById(R.id.button_return);
         buttonReturn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
-            }
-        });
-        Button buttonReplace = findViewById(R.id.button_replace);
-        buttonReplace.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                    themeText.setText("Dark theme");
-
             }
         });
     }
